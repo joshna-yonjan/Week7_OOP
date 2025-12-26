78ea8011e14fc581cdbcd2d32d587b8090383222
joshna yonjan <joshnayonjan@joshnas-MacBook-Air.local>
2025-12-26 07:47:47 +0545

    First push

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+package Tutorial7;
+
+/**
+ * This is a calculator class
+ * @author Joshna Yonjan
+ * @version 1.0
+ */
+		
+
+public class Calculator {
+	/**
+	 * This is a add method
+	 * @param a First number
+	 * @param b Second number
+	 * @return Sum of two numbers
+	 */
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+
+}
